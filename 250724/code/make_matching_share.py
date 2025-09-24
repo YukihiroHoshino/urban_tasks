e94e6b8b4382330b11ef5525053c1e5b75e01b2d
YukihiroHoshino <y.hoshino@css.t.u-tokyo.ac.jp>
2025-09-24 16:56:22 +0900

    macpro

--- 250724/code/make_matching_share.py
+++ 250724/code/make_matching_share.py
@@ -306,12 +306,12 @@ def sample_random_trips(n, trips_df_csv="./trips_df.csv", random_trips_df_csv="r
 # print(random_df.head())
 
 # 使用例
-trips_df = pd.read_csv("250724/data/sunday_trips.csv")
-edg_file_path = "250724/data/edge_IC.edg.xml"
-net_file_path = "250724/data/master_forResearch_fixed_after_IC_truck_jp.net.xml"
+trips_df = pd.read_csv("250724/data/thursday_trips.csv")
+edg_file_path = "250724/data/edge_step1.edg.xml"
+net_file_path = "250724/data/master_forResearch_fixed_bukai_step1_truck_jp.net.xml"
 
-rou_file_path = "250724/data/sunday_IC_matched.rou.xml"
-csv_file_path = "250724/data/sunday_IC_matched.csv"
+rou_file_path = "250724/data/thursday_step1_matched.rou.xml"
+csv_file_path = "250724/data/thursday_step1_matched.csv"
 
 processor = ETCDataProcessor(trips_df, edg_file_path, net_file_path, csv_file_path)
 processor.process(rou_file_path)
